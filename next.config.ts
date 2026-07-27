6fac3377df4a15d665f38b38da0b72669cad7301
Thiago Freitas <thiagofreitasdelima9@gmail.com>
2026-07-27 09:55:32 -0300

    test.correçao

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  outputFileTracingIncludes: {
+    "/api/scraper": [
+      "./node_modules/playwright-core/**/*",
+      "./node_modules/@sparticuz/chromium/**/*",
+    ],
+  },
 };
 
 export default nextConfig;
